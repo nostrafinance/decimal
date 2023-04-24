87d9bf51d450e1fef64d62ef8e86e337a6a8087e
Vukašin Manojlović <iamvukasin@gmail.com>
2023-04-24 15:34:51 +0200

    Add formatting class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import Decimal from './decimal';
 
 export default Decimal;
 export { BigNumberish, Numberish } from './decimal';
+export { DecimalFormat } from './format';
 export { abs, decreasePrecision, increasePrecision } from './utils';
