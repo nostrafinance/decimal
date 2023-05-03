a299ac17c33481350d0a8f8deabc3e6988809d57
Lazar <Lazar__93@hotmail.com>
2023-05-03 12:38:45 +0200

    Add npm package support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Decimal from './decimal';
 
-export default Decimal;
-export { BigNumberish, Numberish } from './decimal';
+export type { BigNumberish, Numberish } from './decimal';
+
 export { DecimalFormat } from './format';
+export { Decimal };
+
 export { abs, decreasePrecision, increasePrecision } from './utils';
